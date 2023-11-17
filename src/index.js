3a79d1fa6d42233650d7761444cb0b3bacf59530
Premkumar <prempk1203@gmail.com>
2023-11-17 10:28:56 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function MainHeader(){
+  return(<h1>React Course</h1>)
+}
+
+function SubHeader(){
+  return(<p>This will be a exciting course</p>)
+}
+
+function Header(){
+  return(
+    <div>
+      <MainHeader></MainHeader>
+      <SubHeader></SubHeader>
+    </div>
+  )
+}
+
+function MainBody(){
+  return (
+    <div>
+      <p>In this course we are building TaskoPedia</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to walmart</li>
+      </ul>
+    </div>
+  );
+}
+
+function Footer(){
+  return(<p>Happy Coding</p>)
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Header />
+    <MainBody />
+    <Footer />
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
